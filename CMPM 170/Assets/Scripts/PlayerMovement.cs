7d6ad2f8fcef5846dba65b72f42b863a687bef21
Adrian <adlvasqu@ucsc.com>
2021-11-07 12:02:07 -0800

    Finished polished map. Needs baking

--- CMPM 170/Assets/Scripts/PlayerMovement.cs
+++ CMPM 170/Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,8 @@ public class PlayerMovement : MonoBehaviour
     public GameObject East;
     public GameObject West;
     public GameObject Bonfire;
+    public float slopeForce;
+    public float slopeForceRayLength;
 
     private float gravity;
     private Vector3 direction;
@@ -35,16 +37,30 @@ public class PlayerMovement : MonoBehaviour
             gravity = 0;
         direction = new Vector3(direction.x, gravity, direction.z);
 
+        if((x != 0 || z != 0) && onSlope()){
+            controller.Move(Vector3.down * controller.height/2 * slopeForce * Time.deltaTime);
+            Debug.Log("Working");
+        }
+
         Vector3 move = direction;
         controller.Move(move * speed * Time.deltaTime);
-        if(teleport()){
-            Debug.Log("Teleported!");
+
+        teleport();
+        //if(teleport()){
+            //Debug.Log("Teleported!");
             //if teleported do...
-            Instantiate(Bonfire, transform.position, Quaternion.identity);  //just an example
-        }
+            //Instantiate(Bonfire, transform.position, Quaternion.identity);  //just an example
+        //}
     }
     private void groundCheck(){
         isGrounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+    public bool onSlope(){
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, (controller.height/2) * slopeForceRayLength)){
+            if(hit.normal != Vector3.up) return true;
+        }
+        return false;
     }
 
     //currently i am still trying to figure out how to use collision 
@@ -53,38 +69,44 @@ public class PlayerMovement : MonoBehaviour
         
         if(other.CompareTag("Marker") && center == false){
             Debug.Log("collider");
-            Instantiate(Bonfire, other.transform.position + other.transform.up * 23, Quaternion.identity);
+            //Instantiate(Bonfire, other.transform.position + other.transform.up * 23, Quaternion.identity);
             center = true;
         }
 
         if(other.CompareTag("Bonfire") && center == true){
             Debug.Log("bonfire lit");
             center = false;
-            Destroy(GameObject.FindWithTag("Bonfire"));
+            //Destroy(GameObject.FindWithTag("Bonfire"));
         }
 
     }
 
     //separate function for teleporting
     //once you near this side, you go to the opposite side. c.x
-    bool teleport(){
+    public void teleport(){
         if(transform.position.x > East.transform.position.x){
-            transform.position = new Vector3(West.transform.position.x, transform.position.y, transform.position.z);
-            return true;
+            transform.position = new Vector3((transform.position.x - East.transform.position.x) + West.transform.position.x, 
+                                             transform.position.y, 
+                                             transform.position.z);
+           // return true;
         }
         if(transform.position.x < West.transform.position.x){
-            transform.position = new Vector3(East.transform.position.x, transform.position.y, transform.position.z);
-            return true;
+            transform.position = new Vector3((transform.position.x - West.transform.position.x) + East.transform.position.x, 
+                                             transform.position.y, 
+                                             transform.position.z);
+           // return true;
         }
-        if(transform.position.z < South.transform.position.z){
-            transform.position = new Vector3(transform.position.x, transform.position.y, North.transform.position.z);
-            return true;
+        if(transform.position.z < South.transform.position.z ){
+            transform.position = new Vector3(transform.position.x, transform.position.y, 
+                                            (transform.position.z - South.transform.position.z) + North.transform.position.z);
+            //return true;
         }
         if(transform.position.z > North.transform.position.z){
-            transform.position = new Vector3(transform.position.x, transform.position.y, South.transform.position.z);
-            return true;
+            transform.position = new Vector3(transform.position.x, transform.position.y, 
+                                            (transform.position.z - North.transform.position.z) + South.transform.position.z);
+           // return true;
         }
-        return false;
+       // return false;
     }
 
 }
